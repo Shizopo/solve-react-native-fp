1a1a0d67b8d7731df1efad1bf72eeea3d2af5744
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2019-11-15 12:02:00 +0200

    fix problems with react navigation

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  */
 
 import { AppRegistry } from "react-native";
+import React from "react";
 import App from "./src/components/App/App";
 import { name as appName } from "./app.json";
+import { Provider } from "react-redux";
+import { store } from "./src/configs/createStore";
 
-AppRegistry.registerComponent(appName, () => App);
+console.disableYellowBox = true;
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,59 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+// @flow
 
 import React from "react";
-import Auth from "../Auth";
 import StartScreen from "../StartScreen";
-import ChatList from "../ChatList";
-import ChatScreen from "../ChatScreen";
-import { Provider } from "react-redux";
-import { store } from "../../configs/createStore";
 
-import "react-native-gesture-handler";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-
-type Props = {};
-
-type State = {};
-
-const AppNavigator = createStackNavigator(
-  {
-    Home: StartScreen,
-    Auth: Auth,
-    ChatList: ChatList,
-    Chat: ChatScreen,
-    // Contacts: ContactsList,
-    // Settings: Settings,
-  },
-  {
-    initialRouteName: "Home",
-  }
-);
-
-const AppContainer = createAppContainer(AppNavigator);
-
-class App extends React.Component<Props, State> {
-  render() {
-    return (
-      <>
-        <Provider store={store}>
-          <AppContainer />
-          {/* <Auth /> */}
-          {/* <StartScreen /> */}
-          {/* <ChatList /> */}
-          {/* <ChatScreen /> */}
-        </Provider>
-      </>
-    );
-  }
-}
-
-// export default createAppContainer(AppNavigator);
+const App = () => {
+  return <StartScreen />;
+};
 
 export default App;
